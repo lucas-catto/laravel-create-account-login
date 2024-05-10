75aca1c266ead3ca29d7349abdbee1975fe2ae8b
lucas-catto <lucascattosilva2018@gmail.com>
2024-05-10 11:57:21 -0300

    Create Account =>  View

--- laravel-create-account-login/resources/views/Create_account/create_account.blade.php
+++ laravel-create-account-login/resources/views/Create_account/create_account.blade.php
@@ -1,1 +1,37 @@
-<h1>Create Account</h1>
+
+@extends('Templates/master')
+
+@section('title', 'Home')
+
+@section('links')
+<link rel="stylesheet" href="assets/css/Create_account/create_account.css">
+@endsection
+
+@section('content')
+<main>
+    <form action="#" method="post">
+        
+        <fieldset>
+            <legend>Name</legend>
+            <input type="text" name="name" placeholder="Name">
+        </fieldset>
+
+        <fieldset>
+            <legend>Name</legend>
+            <input type="text" name="username" placeholder="Username">
+        </fieldset>
+        
+        <fieldset>
+            <legend>Name</legend>
+            <input type="email" name="email" placeholder="Email">
+        </fieldset>
+        
+        <fieldset>
+            <legend>Name</legend>
+            <input type="password" name="password" placeholder="Password">
+        </fieldset>
+
+        <button type="submit">Create Account</button>
+    </form>
+</main>
+@endsection
